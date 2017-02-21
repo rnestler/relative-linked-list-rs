7456cdd5e40f6cde366f81508c6b24379ade5084
Raphael Nestler <raphael.nestler@gmail.com>
2017-02-21 13:13:56 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,55 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Node {
-    data: i32,
-    next: Option<usize>,
+pub enum Node<T> {
+    Empty{next: Option<usize>},
+    Used{data: T, next: Option<usize>},
 }
 
 #[derive(Debug)]
-pub struct LinkedList {
-    data: Vec<Node>,
+pub struct LinkedList<T> {
+    data: Vec<Node<T>>,
     head: Option<usize>,
     empty_head: Option<usize>,
 }
 
-pub struct Iter<'a> {
-    data: &'a Vec<Node>,
+pub struct Iter<'a, T:'a> {
+    data: &'a Vec<Node<T>>,
     current_index: Option<usize>,
 }
 
-impl<'a> Iterator for Iter<'a> {
-    type Item = i32;
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = T;
 
-    fn next(&mut self) -> Option<i32> {
+    fn next(&mut self) -> Option<T> {
         if let Some(index) = self.current_index {
             let item = &self.data[index];
-            self.current_index = item.next;
-            Some(item.data)
+            //self.current_index = item.next;
+            //Some(item.data)
+            None
         } else {
             None
         }
     }
 }
 
-impl LinkedList {
-    pub fn new(size: usize) -> LinkedList {
-        let mut list = LinkedList {
+impl<T> LinkedList<T> {
+    pub fn new(size: usize) -> LinkedList<T> {
+        let mut list = LinkedList::<T> {
             data: Vec::with_capacity(size),
             head: None,
             empty_head: None,
         };
         // create the empty list
         for n in 0usize..size {
-            list.data.push(Node{data: 0, next: Some(n+1)});
+            //list.data.push(Node::<T>::Empty{next: Some(n+1)});
         }
         if size > 0 {
             list.empty_head = Some(0);
-            list.data[size-1] = Node{data: 0, next: None};
+            //list.data[size-1] = Node::<T>Empty{data: 0, next: None};
         }
         list
     }
 
-    pub fn iter(&self) -> Iter {
+    pub fn iter(&self) -> Iter<T> {
         Iter {
             data: &self.data,
             current_index: self.head,
@@ -59,21 +60,21 @@ impl LinkedList {
         let slot = self.get_empty().expect("No more slots");
         if let Some(head) = self.head {
             self.head = Some(slot);
-            self.data[slot].data = data;
-            self.data[slot].next = Some(head);
+            //self.data[slot].data = data;
+            //self.data[slot].next = Some(head);
         } else {
             self.head = Some(slot);
-            self.data[slot] = Node{data: data, next: None};
+            //self.data[slot] = Node{data: data, next: None};
         }
     }
 
-    pub fn get_head(&self) -> Option<&Node> {
+    pub fn get_head(&self) -> Option<&Node<T>> {
         self.head.map(|h| &self.data[h])
     }
 
     fn get_empty(&mut self) -> Option<usize> {
         if let Some(empty) = self.empty_head {
-            self.empty_head = self.data[empty].next;
+            //self.empty_head = self.data[empty].next;
             Some(empty)
         } else {
             None
@@ -81,11 +82,11 @@ impl LinkedList {
     }
 }
 
-impl<'a> IntoIterator for &'a LinkedList {
-    type Item = i32;
-    type IntoIter = Iter<'a>;
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type Item = T;
+    type IntoIter = Iter<'a, T>;
 
-    fn into_iter(self) -> Iter<'a> {
+    fn into_iter(self) -> Iter<'a, T> {
         self.iter()
     }
 }
@@ -96,13 +97,13 @@ mod tests {
 
     #[test]
     fn create_empty_list() {
-        let list = LinkedList::new(0);
+        let list = LinkedList::<i32>::new(0);
         assert_eq!(None, list.get_head());
     }
 
     #[test]
     fn create_list() {
-        let list = LinkedList::new(10);
+        let list = LinkedList::<i32>::new(10);
         assert_eq!(None, list.get_head());
     }
 
@@ -111,8 +112,13 @@ mod tests {
         let mut list = LinkedList::new(10);
         list.push(10);
         let head = list.get_head().unwrap();
-        assert_eq!(None, head.next);
-        assert_eq!(10, head.data);
+        match head {
+            &Node::Used{data, next} => {
+                assert_eq!(None, next);
+                assert_eq!(10, data);
+            },
+            _ => panic!(""),
+        }
     }
 
     #[test]
@@ -121,8 +127,15 @@ mod tests {
         list.push(10);
         list.push(20);
         let head = list.get_head().unwrap();
-        assert_eq!(20, head.data);
-        assert!(head.next.is_some());
+        match head {
+            &Node::Used{data, next} => {
+                assert!(next.is_some());
+                assert_eq!(20, data);
+            },
+            _ => panic!(""),
+        }
+        //assert_eq!(20, head.data);
+        //assert!(head.next.is_some());
     }
 
     #[test]
